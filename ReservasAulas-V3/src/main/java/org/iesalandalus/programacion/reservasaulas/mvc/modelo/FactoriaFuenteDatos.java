4f73b3442e3a0cc87525961b496c03b1d0c954dc
danielcalatravagonzalez <degixcala@gmail.com>
2022-03-11 19:15:56 +0100

    Ejercicio 8: He actualizado FactoriaFuenteDatos para que pueda crear de la opción ficheros.

--- ReservasAulas-V3/src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/modelo/FactoriaFuenteDatos.java
+++ ReservasAulas-V3/src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/modelo/FactoriaFuenteDatos.java
@@ -1,11 +1,17 @@
 package org.iesalandalus.programacion.reservasaulas.mvc.modelo;
 
+import org.iesalandalus.programacion.reservasaulas.mvc.modelo.negocio.ficheros.FactoriaFuenteDatosFicheros;
 import org.iesalandalus.programacion.reservasaulas.mvc.modelo.negocio.memoria.FactoriaFuenteDatosMemoria;
 
 public enum FactoriaFuenteDatos {
 	MEMORIA {
 		public IFuenteDatos crear() {
 			return new FactoriaFuenteDatosMemoria();
+		}
+	},
+	FICHEROS {
+		public IFuenteDatos crear() {
+			return new FactoriaFuenteDatosFicheros();
 		}
 	};
 	
